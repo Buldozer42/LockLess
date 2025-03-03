7d4ee461fe55023a6ad9ae0ec320ea1fafec9e3c
Noé Garnier <Noe.GARNIER@etu.uca.fr>
2025-03-03 18:16:01 +0100

    Database connection

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const { connectDB } = require('./service/db');
+
+app.use(express.json());
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
