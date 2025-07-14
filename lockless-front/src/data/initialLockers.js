1edd1c6c72810c7b7140c89ec78c471a830469ca
sandara.ly <sandara.ly@sully-group.fr>
2025-07-14 16:01:38 +0200

    add front end

--- lockless-front/src/data/initialLockers.js
+++ lockless-front/src/data/initialLockers.js
@@ -1,16 +1,16 @@
 const initialLockers = [
-  { id: 1, number: 1, size: 'small', price: '€', status: 'available' },
-  { id: 2, number: 2, size: 'medium', price: '€€', status: 'available' },
-  { id: 3, number: 3, size: 'large', price: '€€€', status: 'available' },
-  { id: 4, number: 4, size: 'small', price: '€', status: 'available' },
-  { id: 5, number: 5, size: 'large', price: '€€', status: 'available' },
-  { id: 6, number: 6, size: 'medium', price: '€', status: 'available' },
-  { id: 7, number: 7, size: 'small', price: '€', status: 'available' },
-  { id: 8, number: 8, size: 'large', price: '€€€', status: 'available' },
-  { id: 9, number: 9, size: 'medium', price: '€€', status: 'available' },
-  { id: 10, number: 10, size: 'large', price: '€€', status: 'available' },
-  { id: 11, number: 11, size: 'small', price: '€', status: 'available' },
-  { id: 12, number: 12, size: 'medium', price: '€€', status: 'available' }
+  { id: 1, number: 1, size: 'small', price: 7, status: 'available' },
+  { id: 2, number: 2, size: 'medium', price: 10, status: 'available' },
+  { id: 3, number: 3, size: 'large', price: 12, status: 'available' },
+  { id: 4, number: 4, size: 'small', price: 7, status: 'available' },
+  { id: 5, number: 5, size: 'large', price: 12, status: 'available' },
+  { id: 6, number: 6, size: 'medium', price: 10, status: 'available' },
+  { id: 7, number: 7, size: 'small', price: 7, status: 'available' },
+  { id: 8, number: 8, size: 'large', price: 12, status: 'available' },
+  { id: 9, number: 9, size: 'medium', price: 10, status: 'available' },
+  { id: 10, number: 10, size: 'large', price: 12, status: 'available' },
+  { id: 11, number: 11, size: 'small', price: 7, status: 'available' },
+  { id: 12, number: 12, size: 'medium', price: 10, status: 'available' }
 ];
 
 export default initialLockers;
